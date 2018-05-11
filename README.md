424b17f8c6850a32e924c5efd988bd20f07a4b7a
biturbo <serge.guex@hotmail.com>
2018-05-11 13:28:19 +0200

    inital release

---
+++ README.md
@@ -0,0 +1,38 @@
+An alternative method for installing Stratux on your board's Linux OS.
+
+The script is currently in beta development.
+
+Commands to run the setup script:
+
+    [login via command line]
+
+    # sudo su -
+
+    [Raspberry Pi boards]
+
+    # raspi-config
+        select option 1 - expand filesystem
+        reboot
+
+    # cd /root
+
+    # git clone https://github.com/biturbo/stratuxflarm-setup
+    # cd stratuxflarm-setup
+
+    # bash stratuxflarm-setup.sh
+        - currently detected boards: RPi3
+        - note, the setup script performs a dist-upgrade, if it's the
+        first time the setup may take a considerable amount of time
+
+    # reboot
+
+
+Requirements:
+
+    - Linux compatible board
+    - Linux OS
+    - apt-get
+    - ethernet connection
+    - wifi
+    - keyboard
+    - a little command line fu
---
+++ rpi.sh
@@ -0,0 +1,124 @@
+# Copyright (c) 2016 Joseph D Poirier
+# Distributable under the terms of The New BSD License
+# that can be found in the LICENSE file.
+
+echo "${MAGENTA}"
+echo "************************************"
+echo "****** Raspberry Pi setup... *******"
+echo "************************************"
+echo "${WHITE}"
+
+
+# TODO: rpi0 checks and setup?
+
+##############################################################
+##  Boot config settings
+##############################################################
+echo
+echo "${YELLOW}**** Boot config settings... *****${WHITE}"
+
+if ! grep -q "dtparam=audio=on" "/boot/config.txt"; then
+    echo "dtparam=audio=on" >>/boot/config.txt
+fi
+
+if [ "$REVISION" == "$RPI2BxREV" ] || [ "$REVISION" == "$RPI2ByREV" ] || [ "$REVISION" == "$RPI0xREV" ] || [ "$REVISION" == "$RPI0yREV" ]; then
+    if ! grep -q "max_usb_current=1" "/boot/config.txt"; then
+        echo "max_usb_current=1" >>/boot/config.txt
+    fi
+fi
+
+if ! grep -q "dtparam=i2c1=on" "/boot/config.txt"; then
+    echo "dtparam=i2c1=on" >>/boot/config.txt
+fi
+
+if ! grep -q "dtparam=i2c1_baudrate=400000" "/boot/config.txt"; then
+    echo "dtparam=i2c1_baudrate=400000" >>/boot/config.txt
+fi
+
+if ! grep -q "dtparam=i2c_arm_baudrate=400000" "/boot/config.txt"; then
+    echo "dtparam=i2c_arm_baudrate=400000" >>/boot/config.txt
+fi
+
+if [ "$REVISION" == "$RPI3BxREV" ] || [ "$REVISION" == "$RPI3ByREV" ]; then
+    # move RPi3 Bluetooth off of hardware UART to free up connection for GPS
+    if ! grep -q "dtoverlay=pi3-miniuart-bt" "/boot/config.txt"; then
+        echo "dtoverlay=pi3-miniuart-bt" >>/boot/config.txt
+    fi
+fi
+
+if ! grep -q "dtparam=act_led_trigger=none" "/boot/config.txt"; then
+    echo "dtparam=act_led_trigger=none" >>/boot/config.txt
+fi
+
+if ! grep -q "dtparam=act_led_activelow=off" "/boot/config.txt"; then
+    echo "dtparam=act_led_activelow=off" >>/boot/config.txt
+fi
+
+#echo "arm_freq=900" >>boot/config.txt
+#echo "sdram_freq=450" >>boot/config.txt
+#echo "core_freq=450" >>boot/config.txt
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Disable serial console
+##############################################################
+echo
+echo "${YELLOW}**** Disable serial console... *****${WHITE}"
+
+sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  I2C setup
+##############################################################
+echo
+echo "${YELLOW}**** I2C setup... *****${WHITE}"
+
+if ! grep -q "i2c-bcm2708" "/etc/modules"; then
+    echo "i2c-bcm2708" >>/etc/modules
+fi
+
+if ! grep -q "i2c-dev" "/etc/modules"; then
+    echo "i2c-dev" >>/etc/modules
+fi
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Sysctl tweaks
+##############################################################
+echo
+echo "${YELLOW}**** Sysctl tweaks... *****${WHITE}"
+
+if grep -q "net.core.rmem_max" "/etc/sysctl.conf"; then
+    line=$(grep -n 'net.core.rmem_max' /etc/sysctl.conf | awk -F':' '{print $1}')d
+    sed -i $line /etc/sysctl.conf
+fi
+
+if grep -q "net.core.rmem_default" "/etc/sysctl.conf"; then
+    line=$(grep -n 'net.core.rmem_default' /etc/sysctl.conf | awk -F':' '{print $1}')d
+    sed -i $line /etc/sysctl.conf
+fi
+
+if grep -q "net.core.wmem_max" "/etc/sysctl.conf"; then
+    line=$(grep -n 'net.core.wmem_max' /etc/sysctl.conf | awk -F':' '{print $1}')d
+    sed -i $line /etc/sysctl.conf
+fi
+
+if grep -q "net.core.wmem_default" "/etc/sysctl.conf"; then
+    line=$(grep -n 'net.core.wmem_default' /etc/sysctl.conf | awk -F':' '{print $1}')d
+    sed -i $line /etc/sysctl.conf
+fi
+
+echo "net.core.rmem_max = 167772160" >>/etc/sysctl.conf
+echo "net.core.rmem_default = 167772160" >>/etc/sysctl.conf
+echo "net.core.wmem_max = 167772160" >>/etc/sysctl.conf
+echo "net.core.wmem_default = 167772160" >>/etc/sysctl.conf
+
+echo "${GREEN}...done${WHITE}"
+
